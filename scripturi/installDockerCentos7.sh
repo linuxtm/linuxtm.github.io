441a2e5bf46b41758f9453e9f8faf639e553d95f
Marc Iulian <linuxtm@users.noreply.github.com>
2020-02-01 11:41:25 +0200

    Create installDockerCentos7.sh

---
+++ scripturi/installDockerCentos7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+
+#Install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
