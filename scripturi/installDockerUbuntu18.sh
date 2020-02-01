48f5bba5e44f56345a39265bf816aa606d172dee
Marc Iulian <linuxtm@users.noreply.github.com>
2020-02-01 11:33:29 +0200

    Create installDockerUbuntu18.sh

---
+++ scripturi/installDockerUbuntu18.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+no=$(tput sgr0)
+red=$(tput setaf 1)
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo usermod -aG docker $USER
+sudo systemctl enable docker && sudo systemctl start docker
+
+#Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Done."
+echo -e "${red}In order for docker to work as a non-root user, please logout or restart your computer!${no}"
