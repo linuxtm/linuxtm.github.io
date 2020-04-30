70f6ba7d42403e92439869b8610b674d8825c3a0
Marc Iulian <linuxtm@users.noreply.github.com>
2020-04-30 13:08:12 +0300

    Update installDockerUbuntu18.sh

--- scripturi/installDockerUbuntu18.sh
+++ scripturi/installDockerUbuntu18.sh
@@ -18,7 +18,7 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker && sudo systemctl start docker
 
 #Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Done."
