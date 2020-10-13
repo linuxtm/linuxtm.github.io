f5fea148647a0dc4e92315726950c661da4afa64
Marc Iulian <linuxtm@users.noreply.github.com>
2020-10-13 08:36:04 +0300

    Update installDockerUbuntu20.sh

--- scripturi/installDockerUbuntu20.sh
+++ scripturi/installDockerUbuntu20.sh
@@ -19,7 +19,7 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker && sudo systemctl start docker
 
 #Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Done."
